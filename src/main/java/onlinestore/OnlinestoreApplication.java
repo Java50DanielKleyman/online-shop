28f9000fcff185c6820e4d0e9fcb359ea4cdb869
Daniel Kleyman <denny_klm1@yahoo.com>
2024-02-22 14:30:08 +0200

    java-17 changes in pom.xml

--- src/main/java/onlinestore/OnlinestoreApplication.java
+++ src/main/java/onlinestore/OnlinestoreApplication.java
@@ -2,12 +2,21 @@ package onlinestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class OnlinestoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlinestoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinestoreApplication.class, args);
+    }
 
+    @GetMapping("/")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello, world!";
+    }
 }
